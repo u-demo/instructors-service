f490f6b89ccee7c2f3d11a1e85168b7af8026552
Charlie Axelrod <charlieaxelrod@gmail.com>
2018-10-29 15:18:04 -0700

    Fetches data from database

--- server/index.js
+++ server/index.js
@@ -1,14 +1,39 @@
 const express = require('express');
-const mysql = require('../database/index.js');
+const mysql = require('../database/sqlizeIndex.js');
+
 const app = express();
 
 
 app.use(express.static(__dirname + '/../client/dist'));
-app.get('/instructors', (req, res) => {
-  mysql.query('SELECT * FROM instructors', (error, results, fields) => {
-    if (error) throw error;
-    res.json(results);
-  });
+app.get('/instructors/:id', (req, res) => {
+  mysql.sequelize.authenticate()
+    .then(() => mysql.Join.findAll({ where: { course_id: req.params.id } }))
+    .then((data) => {
+      const info = [];
+      const promises = [];
+      data.forEach((inst) => {
+        const instructor = ({
+          id: inst.dataValues.inst_id,
+          instInfo: null,
+          courseInfo: null,
+        });
+        const newPromise = mysql.Instructors.findOne({ where: { id: inst.dataValues.inst_id } })
+          .then((instData) => {
+            instructor.instInfo = instData;
+            return mysql.Join.findAll({ where: { inst_id: inst.dataValues.inst_id } });
+          })
+          .then(courses => mysql.Courses.findAll({
+            where: { id: [courses.map(course => course.course_id)] },
+          }))
+          .then((courseData) => {
+            instructor.courseInfo = courseData;
+            info.push(instructor);
+          });
+        promises.push(newPromise);
+      });
+      return Promise.all(promises)
+        .then(() => res.send(info));
+    });
 });
 
 app.listen(3000, () => {
